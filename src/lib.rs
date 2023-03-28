abffbabf422c52df182fa4aae4fe7bfce91764da
Gustavo Meira <grnmeira@gmail.com>
2023-03-28 22:23:00 +0100

    finishing up quick cli

--- src/lib.rs
+++ src/lib.rs
@@ -3,44 +3,77 @@ pub mod webex {
         use serde::Deserialize;
         use tokio::time::{self, Duration, Instant};
 
-        const CLIENT_ID: &str = "";
-        const CLIENT_SECRET: &str = "";
         const SCOPE: &str = "spark:all";
         const GRANT_TYPE: &str = "urn:ietf:params:oauth:grant-type:device_code";
 
         #[derive(Debug)]
         pub struct DeviceAuthenticator {
             client_id: String,
-            client_secret: String
+            client_secret: String,
         }
 
+        #[allow(dead_code)]
         #[derive(Debug)]
         pub struct VerificationToken {
+            code: String,
+            verification_uri: String,
             verification_uri_complete: String,
+            polling_interval: u64,
         }
 
         #[derive(Debug)]
-        pub enum VerificationError {
+        pub enum Error {
             HttpRequestError,
             ParsingError,
-            GenericError
+            GenericError,
         }
 
-        impl From<reqwest::Error> for VerificationError {
-            fn from(error: reqwest::Error) -> VerificationError {
-                return VerificationError::HttpRequestError;
+        // This can be easily done with the "thiserror" crate,
+        // but I'll have some fun as I'm not fully familiar with
+        // the language yet.
+        impl From<reqwest::Error> for Error {
+            fn from(error: reqwest::Error) -> Error {
+                if error.is_status() {
+                    Error::HttpRequestError
+                } else if error.is_decode() {
+                    Error::ParsingError
+                } else {
+                    Error::GenericError
+                }
             }
+        }
+
+        #[allow(dead_code)]
+        #[derive(Deserialize, Debug)]
+        struct AuthorizeResponse {
+            device_code: String,
+            expires_in: u64,
+            user_code: String,
+            verification_uri: String,
+            verification_uri_complete: String,
+            interval: u64,
+        }
+
+        #[derive(Deserialize, Debug)]
+        struct TokenResponse {
+            access_token: String,
+        }
+
+        #[derive(Debug)]
+        pub enum DeviceAuthenticationError {
+            HttpError(u16),
+            UnknownError,
         }
 
         impl DeviceAuthenticator {
-            pub fn new(client_id: &str, client_secret: &str) -> DeviceAuthenticator{
-                return DeviceAuthenticator {
+            pub fn new(client_id: &str, client_secret: &str) -> DeviceAuthenticator {
+                DeviceAuthenticator {
                     client_id: client_id.to_string(),
-                    client_secret: client_secret.to_string()
-                };
+                    client_secret: client_secret.to_string(),
+                }
             }
 
-            pub async fn verify(&self) -> Result<VerificationToken, VerificationError> {
+            pub async fn verify(&self) -> Result<VerificationToken, Error> {
                 let client = reqwest::Client::new();
                 let params = [("client_id", self.client_id.as_str()), ("scope", SCOPE)];
                 let response = client
@@ -52,98 +85,57 @@ pub mod webex {
                     .form(&params)
                     .send()
                     .await?;
-                    //.expect("Request to authorization endpoint failed")
-                let token = response.json::<AuthorizeResponse>()
-                    .await?;
 
-                return Ok(VerificationToken{ verification_uri_complete: token.verification_uri_complete });
-            }
-        }
+                let token = response.json::<AuthorizeResponse>().await?;
 
-        #[derive(Deserialize, Debug)]
-        struct AuthorizeResponse {
-            device_code: String,
-            expires_in: u64,
-            user_code: String,
-            verification_uri: String,
-            verification_uri_complete: String,
-            interval: u64,
-        }
-        
-        #[derive(Deserialize, Debug)]
-        struct TokenResponse {
-            access_token: String,
-        }
-        
-        #[derive(Debug)]
-        pub enum DeviceAuthenticationError {
-            HttpError(u16),
-            UnknownError,
-        }
-
-        pub async fn authenticate_device(client_id: &str, client_secret: &str) -> Result<String, DeviceAuthenticationError>
-        {
-            let client = reqwest::Client::new();
-
-            let params = [("client_id", CLIENT_ID), ("scope", SCOPE)];
-            let response = client
-                .post("https://webexapis.com/v1/device/authorize")
-                .header(
-                    reqwest::header::CONTENT_TYPE,
-                    "application/x-www-form-urlencoded; charset=utf-8",
-                )
-                .form(&params)
-                .send()
-                .await
-                .expect("Request to authorization endpoint failed")
-                .json::<AuthorizeResponse>()
-                .await
-                .expect("Authorization response parsing failed");
-        
-            println!("{}", response.user_code);
-            println!("{}", response.verification_uri);
-            println!("{}", response.verification_uri_complete);
-        
-            let params = [
-                ("grant_type", GRANT_TYPE),
-                ("device_code", &response.device_code),
-                ("client_id", CLIENT_ID),
-            ];
-        
-            let mut interval = time::interval_at(
-                Instant::now() + Duration::from_secs(response.interval),
-                Duration::from_secs(response.interval + 1),
-            );
-        
-            loop {
-                interval.tick().await;
-                let response = client
-                    .post("https://webexapis.com/v1/device/token")
-                    .basic_auth(CLIENT_ID, Some(CLIENT_SECRET))
-                    .header(
-                        reqwest::header::CONTENT_TYPE,
-                        "application/x-www-form-urlencoded; charset=utf-8",
-                    )
-                    .form(&params)
-                    .send()
-                    .await
-                    .expect("Request to token endpoint failed");
-        
-                if response.status() == 200 {
-                    let token_response = response
-                        .json::<TokenResponse>()
-                        .await
-                        .expect("Failed to parse Token Response");
-                    println!("{}", token_response.access_token);
-                    return Ok(token_response.access_token);
-                }
-        
-                if response.status() != 428 {
-                    return Err(DeviceAuthenticationError::HttpError(response.status().into()));
-                }
+                Ok(VerificationToken {
+                    code: token.device_code,
+                    verification_uri: token.verification_uri,
+                    verification_uri_complete: token.verification_uri_complete,
+                    polling_interval: token.interval,
+                })
             }
 
-            return Err(DeviceAuthenticationError::UnknownError);
+            pub async fn wait_for_authentication(
+                &self,
+                verification_token: &VerificationToken,
+            ) -> Result<String, Error> {
+                let client = reqwest::Client::new();
+
+                let params = [
+                    ("grant_type", GRANT_TYPE),
+                    ("device_code", &verification_token.code),
+                    ("client_id", &self.client_id),
+                ];
+
+                let mut interval = time::interval_at(
+                    Instant::now() + Duration::from_secs(verification_token.polling_interval),
+                    Duration::from_secs(verification_token.polling_interval + 1),
+                );
+
+                loop {
+                    interval.tick().await;
+                    let response = client
+                        .post("https://webexapis.com/v1/device/token")
+                        .basic_auth(&self.client_id, Some(&self.client_secret))
+                        .header(
+                            reqwest::header::CONTENT_TYPE,
+                            "application/x-www-form-urlencoded; charset=utf-8",
+                        )
+                        .form(&params)
+                        .send()
+                        .await?;
+
+                    if response.status() == 200 {
+                        let token_response = response.json::<TokenResponse>().await?;
+                        return Ok(token_response.access_token);
+                    }
+
+                    if response.status() != 428 {
+                        return Err(Error::HttpRequestError);
+                    }
+                }
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
-use tokio::time::{self, Duration, Instant};
+use clap::Parser;
 use webex_api_device_auth::webex;
 
-const CLIENT_ID: &str = "";
-const CLIENT_SECRET: &str = "";
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short = 'i', help = "client (integration) ID")]
+    client_id: String,
+    #[arg(short = 's', help = "client (integration) secret")]
+    client_secret: String,
+}
 
 #[tokio::main]
 async fn main() {
-    //webex::auth::authenticate_device(CLIENT_ID, CLIENT_SECRET).await.expect("Failure anthenticating device");
-    let authenticator = webex::auth::DeviceAuthenticator::new(CLIENT_ID, CLIENT_SECRET);
+    let args = Args::parse();
+    let authenticator = webex::auth::DeviceAuthenticator::new(&args.client_id, &args.client_secret);
     let verification_token = authenticator.verify().await.unwrap_or_else(|error| {
         panic!("Error obtaining verification token: {:#?}", error);
     });
     println!("{:#?}", verification_token);
-    /*
-    println!("{}", &verification_token.verification_uri);
-    let verification_result = verification_token.wait_for_result().await;
-    println!("{}", verification_result.access_token);
-
-    // renew token
-    // expire token
-    */
+    let bearer_token = authenticator
+        .wait_for_authentication(&verification_token)
+        .await
+        .unwrap_or_else(|error| {
+            panic!("Error obtaining bearer token: {:#?}", error);
+        });
+    println!("Bearer token:");
+    println!("{}", bearer_token);
 }
